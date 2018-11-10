58ab49cc3fe4cd82585c55826e1d9195e4438984
Ziyang Hu <hu.ziyang@cantab.net>
2018-11-10 09:22:49 +0800

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setup(
     name='py-pgtest',
-    version='0.1.7',
+    version='0.1.8',
     packages=['py_pgtest'],
     url='https://github.com/zh217/py-pgtest',
     license='MIT',
